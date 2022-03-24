5975f5b7280821f901f20681aa8e69935da01dc8
Mostafa Ahmed <79556633+mosta1489@users.noreply.github.com>
2022-03-24 14:39:18 +0200

    finish learn

--- models/userModel.js
+++ models/userModel.js
@@ -1,16 +1,38 @@
 const mongoose = require("mongoose");
 
+const adressSchema = new mongoose.Schema({
+  street: String,
+  city: String,
+});
+
 const userSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  email: String,
-  createdAt: Date,
-  updatedAt: Date,
-  hobbies: [String],
-  adress: {
-    street: String,
-    city: String,
+  name: {
+    type: String,
+    required: true,
+    lowercase: true,
   },
+
+  age: {
+    type: Number,
+    min: 1,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  createdAt: {
+    type: Date,
+    // default: new Date(),
+    default: () => Date.now(),
+    immutable: true,
+  },
+  updatedAt: {
+    type: Date,
+    default: () => Date.now(),
+  },
+  hobbies: [String],
+  adress: adressSchema,
 });
 
 module.exports = mongoose.model("user", userSchema);
